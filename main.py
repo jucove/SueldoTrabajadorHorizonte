d2d3abdbeb8c3292615bc41620ad2d2d94f16136
jucove <74031526@continental.edu.pe>
2023-01-29 17:34:59 -0500

    Segundo Refinamiento

--- main.py
+++ main.py
@@ -5,19 +5,28 @@ sueldoBasico = float (input('Ingrese el valor del sueldo basico: '))
 diasFalta = float (input('Ingrese los dias de falta: '))
 minutosTardanza = float (input('Ingrese los minutos de tardanza: '))
 horasExtras = float (input('Ingrese las horas extras: '))
-bonificacionMovilidad = 1000
+Movilidad = 1000
 bonificacionSuplementaria = 0.03*sueldoBasico
-CostoHoraNormal = sueldoBasico/240
-CostoHoraExtra = 1.5*CostoHoraNormal
-bonificacionHoraExtra = horasExtras*CostoHoraExtra
-bonificaciones = bonificacionMovilidad + bonificacionSuplementaria + bonificacionHoraExtra
-descuentos = diasFalta+minutosTardanza
+pagoHorasExtra = 1.5*horasExtras*(sueldoBasico/240)
+bonificaciones = Movilidad + bonificacionSuplementaria + pagoHorasExtra
+remuneracionComputable = sueldoBasico + Movilidad + bonificacionSuplementaria
+remuneracionMinima = sueldoBasico + bonificaciones
+DescuentoFaltas = (remuneracionComputable/30)*diasFalta
+DescuentoTardanzas = (remuneracionComputable/14400)*minutosTardanza
+descuentos = DescuentoFaltas+DescuentoTardanzas
 sueldoNeto = sueldoBasico + bonificaciones-descuentos
 
 os.system ('pause')
 
 print('                         Boleta de pago de '+ nombreTrabajador)
-print('El sueldo basico es: ' + repr (sueldoBasico))
-print('La bonificacion es: ' + repr (bonificaciones))
-print('Los descuentos son:-' + repr (descuentos))
-print('El sueldo neto es: ' + repr (sueldoNeto))
+print('El sueldo basico es:                   +' + repr (sueldoBasico))
+print('La bonificacion suplementario es:      +' + repr (bonificacionSuplementaria))
+print('La bonificacion por movilidad es:      +' + repr (Movilidad))
+HorasExtra = round(pagoHorasExtra, 2)
+print('El pago por horas extras es:           +' + repr (HorasExtra))
+Faltas = round(DescuentoFaltas, 2)
+print('Los descuentos por faltas son:         -' + repr (Faltas))
+Tardanzas = round(DescuentoTardanzas, 2)
+print('Los descuentos por tardanzas son:      -' + repr (Tardanzas))
+Sueldo = round(sueldoNeto, 2)
+print('El sueldo neto es:                     =' + repr (Sueldo))
